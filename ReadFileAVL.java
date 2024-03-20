91ebf1c83e50e275bba69f460f2bde1b3e5151bc
UnathiShabangu <164073097+UnathiShabangu@users.noreply.github.com>
2024-03-20 03:10:24 +0200

    Update ReadFileAVL.java

---
+++ ReadFileAVL.java
@@ -0,0 +1,34 @@
+public class ReadFileAVL {
+    private String fileName;
+    private Scanner fileReader;
+    private AVLTree<WordAVL> wordList;
+    
+    
+    public ReadFileAVL(String fileName)throws FileNotFoundException{
+        try {
+            File file = new File(fileName);
+            Scanner fileReader = new Scanner(file);
+            wordList = new AVLTree<>();
+            appendTree(fileReader);
+            fileReader.close(); 
+        } 
+        catch (FileNotFoundException e) {
+            throw new FileNotFoundException("File not found: " + fileName);
+        }
+   } 
+   public void appendTree(WordAVL w){
+        wordList.insert(w);
+    }
+    //creating a Word object from the String and add it to the wordList tree
+    private void appendTree(Scanner fileReader) {
+        fileReader.useDelimiter("\t");
+        while (fileReader.hasNextLine()) {
+            String word = fileReader.next();
+            String def = fileReader.next();
+            String score = fileReader.nextLine();
+            WordAVL wordOBJ = new WordAVL(word, def, score);
+            wordList.insert(wordOBJ);
+        }
+    }
+}
+    
